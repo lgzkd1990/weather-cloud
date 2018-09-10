140a766a2e14215602613a2b022cac0ce9240c5e
lgzkd <wodeshijie1234@126.com>
2018-09-10 20:16:22 +0800

    第一次构建好工程目录结构

---
+++ src/main/java/com/zl/weathercloud/WeatherCloudApplication.java
@@ -0,0 +1,12 @@
+package com.zl.weathercloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherCloudApplication.class, args);
+    }
+}
